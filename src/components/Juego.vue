8a5ce6832cc020039c43f56431f2620271522442
miguerlfr <146797574+miguerlfr@users.noreply.github.com>
2024-04-16 12:12:44 -0500

    Update and rename Venta.vue to Juego.vue

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -2,25 +2,31 @@
     <q-page-container class="page">
         <section class="contTraer">
             <label class="label" for="nombrePokemon">¿Quién es este Pokémon?</label>
-            <input id="nombrePokemon" v-model="nombreIngresado" class="input" type="text" placeholder="Nombre del Pokémon" />
-            <button class="adivinar" @click="adivinarPokemon()">Adivinar Pokémon</button>
-            <!-- Nuevo botón agregado -->
-            <button class="mostrarDatos" @click="mostrarDatosPokemon()">Mostrar Datos en Consola</button>
-            <!-- Fin de nuevo botón agregado -->
+            <input id="nombrePokemon" v-model="nombreIngresado" class="input" type="text"
+                placeholder="Nombre Pokemon" />
+            <button class="nuevoPokemon" @click="traerProximoPokemon()" :disabled="!puedeMostrarNuevoPokemon">
+                Jugar De Nuevo
+            </button>
+            <button class="verificarPokemon" @click="verificarPokemon()">Verificar</button>
         </section>
         <section v-if="mostrarDatos" class="pageHijo">
             <ul class="pokemons">
-                <img class="pokeGif" v-if="pokemon.sprites && pokemon.sprites.other && pokemon.sprites.other.showdown && pokemon.sprites.other.showdown.front_default" :src="pokemon.sprites.other.showdown.front_default" alt="Pokemon gif 1" />
-                <img class="pokemon" v-if="pokemon.sprites" :src="getShinySpriteUrl(pokemon.sprites)" alt="Shiny Pokemon" />
-
-                <img class="pokeGif" v-if="pokemon.sprites && pokemon.sprites.other && pokemon.sprites.other.showdown && pokemon.sprites.other.showdown.back_default" :src="pokemon.sprites.other.showdown.back_default" alt="Pokemon gif 2" />
+                <img class="pokeGif"
+                    v-if="pokemon.sprites && pokemon.sprites.other && pokemon.sprites.other.showdown && pokemon.sprites.other.showdown.front_default"
+                    :src="pokemon.sprites.other.showdown.front_default" alt="Pokemon gif 1"
+                    :style="{ filter: pokemonColor ? 'brightness(100%)' : 'brightness(0%)' }" />
+                <img class="pokemon" v-if="pokemon.sprites" :src="getShinySpriteUrl(pokemon.sprites)"
+                    alt="Shiny Pokemon" :style="{ filter: pokemonColor ? 'brightness(100%)' : 'brightness(0%)' }" />
+                <img class="pokeGif"
+                    v-if="pokemon.sprites && pokemon.sprites.other && pokemon.sprites.other.showdown && pokemon.sprites.other.showdown.back_default"
+                    :src="pokemon.sprites.other.showdown.back_default" alt="Pokemon gif 2"
+                    :style="{ filter: pokemonColor ? 'brightness(100%)' : 'brightness(0%)' }" />
             </ul>
-
             <div class="container">
                 <ul class="stats-container">
                     <li v-for="(stat, index) in pokemon.stats" :key="index" class="stat">
                         <span>{{ statsTraduccion[stat.stat.name] }}: {{ stat.base_stat }}</span>
-                        <div class="stat-bar" :style="{ width: stat.base_stat * 2 + 'px' }"></div>
+                        <div class="stat-bar" :style="{ width: stat.base_stat * 2.5 + 'px' }"></div>
                     </li>
                 </ul>
             </div>
@@ -32,23 +38,13 @@
 import { ref } from "vue";
 import axios from "axios";
 import { Notify } from "quasar";
-// Variable para controlar si ya se ha adivinado correctamente el Pokémon
-const adivinadoCorrectamente = ref(false);
-// Función para mostrar notificaciones
-const mostrarNotificacion = (mensaje, tipo) => {
-    Notify.create({
-        message: mensaje,
-        color: tipo, // Define el color de la notificación según el tipo
-        position: "bottom", // Posición de la notificación en la pantalla
-        timeout: 2000, // Duración de la notificación en milisegundos
-    });
-};
 
-const mostrarDatosPokemon = () => {
-    // Mostrar los datos del Pokémon en la consola
-    console.log("Datos del Pokémon:", pokemon.value);
-};
-// Define las traducciones y la estructura de datos de los Pokémon
+const mostrarDatos = ref(false);
+const pokemon = ref({});
+const nombreIngresado = ref("");
+const pokemonColor = ref(false); // Variable para controlar si mostrar los Pokémon a color o en blanco y negro
+const puedeMostrarNuevoPokemon = ref(true); // Variable para controlar si se puede mostrar un nuevo Pokémon
+
 const statsTraduccion = {
     hp: "Hp",
     attack: "Ataque",
@@ -79,38 +75,14 @@ const tipoTraduccion = {
     fairy: "Hada",
 };
 
-// Define las referencias reactivas para el Pokémon y la visibilidad de los datos
-const pokemon = ref({
-    name: "",
-    id: null,
-    stats: [],
-    sprites: null,
-    height: null,
-    weight: null,
-    types: [],
-});
-
-const mostrarDatos = ref(false);
-
-// Variable para almacenar el nombre ingresado por el usuario
-const nombreIngresado = ref("");
-
-// Función para obtener la URL de la imagen "back_default" del sprite del Pokémon
-const getShinySpriteUrl = (sprites) => {
-    if (sprites && sprites.other && sprites.other["official-artwork"] && sprites.other["official-artwork"].front_default) {
-        return sprites.other["official-artwork"].front_default;
-    } else {
-        return "";
-    }
-};
-
-// Función para traer los datos del próximo Pokémon
-// Importar axios previamente, por ejemplo: import axios from 'axios';
-
-// Función para traer los datos del próximo Pokémon con axios
 const traerProximoPokemon = async () => {
     try {
-        const numeroPokemon = Math.floor(Math.random() * 898) + 1; // Obtener un número de Pokémon aleatorio entre 1 y 898
+        if (!puedeMostrarNuevoPokemon.value) {
+            mostrarNotificacion("Debes adivinar el Pokémon actual para mostrar otro Pokémon");
+            return;
+        }
+
+        const numeroPokemon = Math.floor(Math.random() * 898) + 1;
         const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${numeroPokemon}`);
         const data = response.data;
         pokemon.value.name = data.name;
@@ -119,44 +91,51 @@ const traerProximoPokemon = async () => {
         pokemon.value.sprites = data.sprites;
         pokemon.value.height = data.height;
         pokemon.value.weight = data.weight;
-        // Mapear y traducir los tipos después de obtener los datos del Pokémon
         pokemon.value.types = data.types.map((type) => {
             return { type: { name: tipoTraduccion[type.type.name] || type.type.name } };
         });
-
         mostrarDatos.value = true;
+        puedeMostrarNuevoPokemon.value = false;
+        console.log("Nombre del Pokémon a adivinar:", pokemon.value.name);
+        // Limpiar el contenido del input
+        nombreIngresado.value = "";
     } catch (error) {
         console.error(error);
     }
 };
 
-// Función para adivinar el Pokémon
-const adivinarPokemon = () => {
+const verificarPokemon = () => {
     const nombreIngresadoLowerCase = nombreIngresado.value.toLowerCase();
     const nombrePokemonLowerCase = pokemon.value.name.toLowerCase();
-
-    if (nombreIngresadoLowerCase === nombrePokemonLowerCase && !adivinadoCorrectamente.value) {
-        // Muestra una notificación de felicitaciones si se adivina correctamente por primera vez
+    if (nombreIngresadoLowerCase === nombrePokemonLowerCase) {
         mostrarNotificacion("¡Felicidades! Adivinaste el Pokémon", "positive");
-        traerProximoPokemon(); // Trae el próximo Pokémon
-        adivinadoCorrectamente.value = true; // Marca que ya se ha adivinado correctamente
-    } else if (nombreIngresadoLowerCase === nombrePokemonLowerCase) {
-        // Trae el próximo Pokémon si ya se ha adivinado correctamente anteriormente
-        traerProximoPokemon();
+        puedeMostrarNuevoPokemon.value = true; // Habilitar el botón "Nuevo Pokémon" nuevamente
     } else {
-        // Muestra una notificación de error si no se adivina correctamente
         mostrarNotificacion("Vuelve a intentar", "negative");
-        mostrarDatosPokemon(); // Muestra los datos del Pokémon en la consola
     }
 };
+
+const getShinySpriteUrl = (sprites) => {
+    if (sprites && sprites.other && sprites.other["official-artwork"] && sprites.other["official-artwork"].front_default) {
+        return sprites.other["official-artwork"].front_default;
+    } else {
+        return "";
+    }
+};
+
+const mostrarNotificacion = (mensaje, tipo) => {
+    Notify.create({
+        message: mensaje,
+        position: "top",
+        color: "white",
+        timeout: 3000,
+    });
+};
+
+traerProximoPokemon();
 </script>
 
 <style>
-* :not(.adivinar):not(.stat):not(.label) {
-    color: white;
-    margin: 0;
-}
-
 .label {
     font-size: 20px;
 }
@@ -169,9 +148,15 @@ const adivinarPokemon = () => {
 }
 
 .input {
-    border: 1px solid v;
-    padding-left: 10px;
+    border: 1px solid black;
+    padding-left: 7.5px;
     width: 130px;
+}
+
+.input .nuevoPokemon .verificar {
+    background-color: white !important;
+    color: black !important;
+    margin: 0 !important;
 }
 
 .color {
@@ -180,13 +165,14 @@ const adivinarPokemon = () => {
 
 .pokemon {
     margin-left: 0.5%;
-    width: 100px !important;
+    width: 320px !important;
     filter: drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.82));
-    margin-bottom: 120px !important;
+    margin-bottom: -80px !important;
+    margin-top: -63px;
 }
 
 .pokeGif {
-    width: 70px;
+    width: 60px;
     filter: drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.82));
 }
 
@@ -214,25 +200,25 @@ const adivinarPokemon = () => {
     flex-direction: row;
     justify-content: center;
     align-items: center;
-    width: 60rem;
-    position: absolute;
+    width: 100%;
+    position: fixed;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     background-color: #50a2efaf;
+    margin-top: 30px;
 }
 
 .pokemons {
-    width: 40rem;
     height: 200px;
     text-align: center;
+    margin-right: 3%;
 }
 
 .container {
     /* Ancho del contenedor */
     display: flex;
     justify-content: center;
-    width: 30rem;
     /* Centra horizontalmente */
 }
 
@@ -242,12 +228,11 @@ const adivinarPokemon = () => {
     justify-content: center;
     align-items: center;
     gap: 10px;
-    padding-top: 20px;
-    padding-bottom: 20px;
+    padding-top: 15px;
+    padding-bottom: 15px;
 }
 
 .stat {
-    padding-right: 200px;
     width: 90%;
     /* Ancho del contenedor de estadísticas */
 }
@@ -255,9 +240,9 @@ const adivinarPokemon = () => {
 .stat-bar {
     /* Ancho máximo de la barra de estadísticas */
     height: 20px;
-    border: 1px solid yellow;
-    background-color: blue;
+    background-color: red;
     flex-grow: 1;
+    border: transparent;
     /* Hace que las barras se expandan automáticamente */
 }
 
@@ -287,6 +272,53 @@ const adivinarPokemon = () => {
 
     100% {
         box-shadow: 0 0 5px rgba(0, 0, 255, 0.7);
+    }
+}
+
+@media (max-width: 950px) or (max-height: 575px) {
+    .pokemon {
+        width: 230px !important;
+        margin-top: -16px;
+    }
+
+    .pokeGif {
+        margin-bottom: -30px;
+    }
+
+    .stats-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        padding-top: 20px;
+        padding-bottom: 30px;
+        font-size: 12px;
+    }
+
+    .stat-bar {
+        height: 10px;
+    }
+}
+
+@media (max-width: 901px) {
+    .pokemon {
+        width: 180px !important;
+        margin-top: 26px;
+    }
+}
+
+@media (max-width: 850px) {
+    .pokemon {
+        margin-left: 260px;
+    }
+
+    .pokeGif {
+        display: none;
+    }
+
+    .stats-container {
+        margin-right: 200px;
     }
 }
 </style>
