bacb1365e48aeb4ff888581e4f8a9cecd5d73a65
miguerlfr <146797574+miguerlfr@users.noreply.github.com>
2024-04-15 21:28:07 -0500

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,10 @@
 import Compra from "../components/Compra.vue"
 import Venta from "../components/Venta.vue"
-import Login from "../components/Login.vue"
 import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
     { path: "/compra", component: Compra },
-    { path: "/venta", component: Venta },
-    { path: "/", component: Login }
+    { path: "/", component: Venta },
 ]
 
 export const router = createRouter({
